5bb0fba109d9e4b2243f35ebdfa9c1ba8c51543c
Luis Ortiz <ortizfrancoluisjavier@gmailcom>
2024-08-27 14:26:02 -0500

    Primer commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { AppPage } from './pages/AppPage';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
+import { theme } from './config/theme';
+
+createRoot(document.getElementById('root')!).render(
+	<StrictMode>
+		<Provider store={store}>
+			<MantineProvider theme={theme}>
+				<AppPage />
+			</MantineProvider>
+		</Provider>
+	</StrictMode>
+);
