32323a48491de9e0a69cc4d1c63669f902b12c76
Maria Alejandra Perdomo <alejandrav3115@gmail.com>
2024-08-29 10:52:51 -0500

    Seperacion de componentes para un codigo mas limpio

---
+++ src/pages/AccessByAction.tsx
@@ -0,0 +1,31 @@
+import { Group, Paper, Stack, Text } from "@mantine/core"
+import { IconMail, IconQrcode } from "@tabler/icons-react"
+import { iconSizes } from "../common/styleConstant"
+import classes from '../common/general.module.css'
+
+interface Props {
+    setAction: (action: string) => void;
+}
+
+export const AccessByAction = (props: Props) => {
+    const { setAction } = props;
+
+    return (
+        <Group grow>
+            <Paper h={{base: '200', sm: 'auto'}} p={'xl'} shadow="md" className={classes.cursorPointer} onClick={()=> setAction('email')}>
+                <Stack gap={0} justify="center" align="center">
+                    <IconMail size={iconSizes.xLarge}/>
+                    <Text fz={'lg'} ta='center'>Correo</Text>
+                    <Text fz='xs' c='dimmed' ta='center'>Ingresa al evento mediante el correo electrónico</Text>
+                </Stack>
+            </Paper>
+            <Paper h={{base: '200', sm: 'auto'}} p={'xl'} shadow="md" className={classes.cursorPointer} onClick={()=> setAction('qr')}>
+                <Stack gap={0} justify="center" align="center">
+                    <IconQrcode size={iconSizes.xLarge}/>
+                    <Text fz={'lg'} ta='center'>Código QR</Text>
+                    <Text fz='xs' c='dimmed' ta='center'>Ingresa al evento mediante el código QR</Text>
+                </Stack>
+            </Paper>
+        </Group>
+    )
+}
--- src/pages/EventCheckin.tsx
+++ src/pages/EventCheckin.tsx
@@ -1,8 +1,9 @@
-import { Anchor, Button, Divider, Group, Paper, Select, Stack, Text, TextInput } from '@mantine/core'
-import { IconAt, IconId, IconMail, IconQrcode } from '@tabler/icons-react'
-import { iconSizes } from '../common/styleConstant'
-import classes from '../common/general.module.css'
-import { gendersLabel } from '../common/gendersLabel';
+import { Button, Group, Stack } from '@mantine/core'
+import { AccessByAction } from './AccessByAction';
+import { AccessByEmail } from './AccessByEmail';
+import { AccessByQR } from './AccessByQR';
+import { RegisterFormByEvent } from './RegisterFormByEvent';
+import { RegisterFormByEventAdditionalFields } from './RegisterFormByEventAdditionalFields';
 
 interface Props {
     action: string;
@@ -18,104 +19,20 @@ export const EventCheckin = (props: Props) => {
     return (
         <>
             {action === '' &&
-                <Group grow>
-                    <Paper h={{base: '200', sm: 'auto'}} p={'xl'} shadow="md" className={classes.cursorPointer} onClick={()=> setAction('email')}>
-                        <Stack gap={0} justify="center" align="center">
-                            <IconMail size={iconSizes.xLarge}/>
-                            <Text fz={'lg'} ta='center'>Correo</Text>
-                            <Text fz='xs' c='dimmed' ta='center'>Ingresa al evento mediante el correo electrónico</Text>
-                        </Stack>
-                    </Paper>
-                    <Paper h={{base: '200', sm: 'auto'}} p={'xl'} shadow="md" className={classes.cursorPointer} onClick={()=> setAction('qr')}>
-                        <Stack gap={0} justify="center" align="center">
-                            <IconQrcode size={iconSizes.xLarge}/>
-                            <Text fz={'lg'} ta='center'>Código QR</Text>
-                            <Text fz='xs' c='dimmed' ta='center'>Ingresa al evento mediante el código QR</Text>
-                        </Stack>
-                    </Paper>
-                </Group>
+                <AccessByAction setAction={setAction} />
             }
             {action === 'email' &&
-                <Stack gap={'xl'} w={'80%'}>
-                    <TextInput 
-                        label='Correo electrónico'
-                        placeholder='tucorreo@gmail.com'
-                        leftSection={<IconAt />}
-                        size='lg'
-                        withAsterisk
-                        error={actionError && <Text ta='center'>No se encuentra registrado, <Anchor onClick={()=>setAction('register')} underline="always">Haz clic aquí para registrarte</Anchor></Text>}
-                    />
-                    <Group justify='end'>
-                        <Button size='lg' onClick={()=> setAction('')} variant='subtle'>Volver</Button>
-                        <Button size='lg' onClick={()=> setAction('')} disabled={actionError}>Ingresar</Button>
-                    </Group>
-                </Stack>
+                <AccessByEmail actionError={actionError} setAction={setAction} />
             }
             {action === 'qr' &&
-                <Stack gap={'xl'} w={'80%'}>
-                    <Text fz={'lg'} fw={500}>Código QR</Text>
-                    <Paper withBorder h={{base: 300, sm: 350}}></Paper>
-                    {actionError && <Text ta='center' c={'red'}>No se encuentra registrado, <Anchor onClick={()=>setAction('register')} underline="always">Haz clic aquí para registrarte</Anchor></Text>}
-                    
-                    <Group justify='end'>
-                        <Button size='lg' onClick={()=> setAction('')} variant='subtle'>Volver</Button>
-                        <Button size='lg' onClick={()=> setAction('')} disabled={actionError}>Ingresar</Button>
-                    </Group>
-                </Stack>
+                <AccessByQR actionError={actionError} setAction={setAction} />
             }
             {action === 'register' &&
                 <Stack gap={'xl'} w={'80%'}>
                     {additionalFields ?
-                        <Stack>
-                            <Divider label={'Campos fijos'} />
-                            <TextInput 
-                                label='Nombre'
-                                placeholder='Nombre'
-                                leftSection={<IconId />}
-                                size='lg'
-                                withAsterisk
-                            />
-                            <TextInput 
-                                label='Correo electrónico'
-                                placeholder='tucorreo@gmail.com'
-                                leftSection={<IconAt />}
-                                size='lg'
-                                withAsterisk
-                            />
-                            <Select
-                                size='lg'
-                                required
-                                label='Género'
-                                placeholder='Seleccione su género'
-                                data={gendersLabel}
-                                clearable
-                            />
-                        </Stack>
+                        <RegisterFormByEvent />
                         :
-                        <Stack>
-                            <Divider label={'Campos adicionales'} />
-                            <TextInput 
-                                label='Nombre'
-                                placeholder='Nombre'
-                                leftSection={<IconId />}
-                                size='lg'
-                            />
-                            <TextInput 
-                                label='Correo electrónico'
-                                placeholder='tucorreo@gmail.com'
-                                leftSection={<IconAt />}
-                                size='lg'
-                                withAsterisk
-                            />
-                            <Select
-                                size='lg'
-                                required
-                                label='Género'
-                                placeholder='Seleccione su género'
-                                data={gendersLabel}
-                                clearable
-                            />
-                        </Stack>
+                        <RegisterFormByEventAdditionalFields />
                     }
                     <Group justify='end'>
                         <Button size='lg' onClick={()=> {setAdditionalFields(true); setAction('')}} variant='subtle'>Volver</Button>
