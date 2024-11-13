6af045d0319d70ee9ad7ffa2398d2f150f8d00a1
Luis Ortiz <ortizfrancoluisjavier@gmail.com>
2024-11-12 22:09:58 -0500

    se optimiza initial options

--- src/pages/initialOptions/InitialOptions.tsx
+++ src/pages/initialOptions/InitialOptions.tsx
@@ -16,49 +16,37 @@ export const InitialOptions = () => {
 
 	const onNext = () => {
 		switch (optionSelected) {
-			case 'code':
+			case OptionSelected.code:
 				goToCheckInWithUserCode();
 				break;
-			case 'qr':
+			case OptionSelected.qr:
 				goToCheckInWithQrCode();
 				break;
-			case 'email':
+			case OptionSelected.email:
 				goToCheckInWithEmail();
 				break;
 		}
 	};
 
 	return (
-		<Container w={'100%'} h={'100%'}>
-			<Stack gap={'xl'} h={'100%'} justify='center'>
-				<Stack>
-					<Text ta={'center'} fz={'lg'} fw={500}>
+		<Container style={{ width: '100%', height: '100%' }}>
+			<Stack gap='xl' style={{ height: '100%' }} justify='center'>
+				<Stack gap='md'>
+					<Text ta='center' size='lg' fw={500}>
 						Identificación del usuario
 					</Text>
-					<Stack>
+					<Stack gap='sm'>
 						<MethodCard
-							image={'/public/assets/login/email_input.svg'}
+							image='/public/assets/login/email_input.svg'
 							label='Correo o código de acceso'
 							isActive={optionSelected === OptionSelected.email}
-							onClic={() => {
-								setOptionSelected(OptionSelected.email);
-							}}
+							onClic={() => setOptionSelected(OptionSelected.email)}
 						/>
-						{/* <MethodCard
-							image={'/public/assets/login/Station_Code.svg'}
-							label='Código de acceso'
-							isActive={optionSelected === OptionSelected.code}
-							onClic={() => {
-								setOptionSelected(OptionSelected.code);
-							}}
-						/> */}
 						<MethodCard
-							image={'/public/assets/login/Station_QR.svg'}
+							image='/public/assets/login/Station_QR.svg'
 							label='QR'
 							isActive={optionSelected === OptionSelected.qr}
-							onClic={() => {
-								setOptionSelected(OptionSelected.qr);
-							}}
+							onClic={() => setOptionSelected(OptionSelected.qr)}
 						/>
 					</Stack>
 				</Stack>
@@ -70,47 +58,4 @@ export const InitialOptions = () => {
 			</Stack>
 		</Container>
 	);
-	/* return (
-		<Stack>
-			<Title order={1} ta={'center'}>
-				Ingreso en {stationType === StationType.event ? 'el evento' : 'la actividad'}
-			</Title>
-			<Group grow>
-				<Paper
-					h={{ base: '200', sm: 'auto' }}
-					p={'xl'}
-					shadow='md'
-					className={classes.cursorPointer}
-					onClick={() => goToCheckInWithEmail()}
-				>
-					<Stack gap={0} justify='center' align='center'>
-						<IconMail size={iconSizes.xLarge} />
-						<Text fz={'lg'} ta='center'>
-							Correo
-						</Text>
-						<Text fz='xs' c='dimmed' ta='center'>
-							Ingresa al evento mediante el correo electrónico
-						</Text>
-					</Stack>
-				</Paper>
-				<Paper
-					h={{ base: '200', sm: 'auto' }}
-					p={'xl'}
-					shadow='md'
-					className={classes.cursorPointer}
-					onClick={() => goToCheckInWithQrCode()}
-				>
-					<Stack gap={0} justify='center' align='center'>
-						<IconQrcode size={iconSizes.xLarge} />
-						<Text fz={'lg'} ta='center'>
-							Código QR
-						</Text>
-						<Text fz='xs' c='dimmed' ta='center'>
-							Ingresa al evento mediante el código QR
-						</Text>
-					</Stack>
-				</Paper>
-			</Group>
-		</Stack>
-	); */
 };
